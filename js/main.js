f332e6d151a9db691debf7b636f15ba6a3cc01e1
jones <selvajones150@gmail.com>
2023-08-01 10:44:41 +0530

    sting method1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var a=prompt("Enter the string");
+var c=[];
+document.write("The given string is: "+a+"<br>")
+var b=a.split("");
+console.log(b);
+for(let i=0;i<b.length;i++){
+    if(b[i]=="a"||b[i]=="e"||b[i]=="i"||b[i]=="o"||b[i]=="u"){
+        c.push(b[i]);
+            }
+}
+document.write("Total count of vowels in the string is "+c.length);
